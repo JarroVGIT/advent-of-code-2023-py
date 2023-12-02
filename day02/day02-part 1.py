72440067cc9b3c2a2f21c8b501e104a4b9745b05
JarroVGIT <jarro@vanginkel-it.nl>
2023-12-02 08:38:16 +0100

    Day 2 solution

--- day02/day02-part 1.py
+++ day02/day02-part 1.py
@@ -8,6 +8,26 @@
 # import re
 from rich import print
 
-with open("./day02/example.txt") as f:
-    # with open("./day02/input.txt") as f:
+# with open("./day02/example.txt") as f:
+with open("./day02/input.txt") as f:
     content = f.read().split("\n")
+
+bag = {"red": 12, "green": 13, "blue": 14}
+
+games_possible = []
+
+for line in content:
+    # Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    split = line.split(": ")
+    game_id = split[0].split(" ")[1]
+    hands = split[1].replace(";", ",").split(",")
+    possible = True
+    for hand in hands:
+        amount, color = hand.split()
+        if bag[color] < int(amount):
+            possible = False
+            break
+    if possible:
+        games_possible.append(int(game_id))
+
+print(f"Part 1: {sum(games_possible)}")
--- day02/day02-part 2.py
+++ day02/day02-part 2.py
@@ -4,10 +4,28 @@
 
 # Possible imports required:
 # import pandas as pd
-# import numpy as np
+import numpy as np
 # import re
 from rich import print
+from collections import defaultdict
 
-with open("./day02/example.txt") as f:
-    # with open("./day02/input.txt") as f:
+# with open("./day02/example.txt") as f:
+with open("./day02/input.txt") as f:
     content = f.read().split("\n")
+
+powers = []
+
+for line in content:
+    # Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    split = line.split(": ")
+    game = split[0].split(" ")[1]
+    hands = split[1].replace(";", ",").split(",")
+    possible_bag = defaultdict(lambda: -1)
+    for hand in hands:
+        amount, color = hand.split()
+        if possible_bag[color] < int(amount):
+            possible_bag[color] = int(amount)
+    power = np.prod([abs(x) for x in possible_bag.values()])
+    powers.append(power)
+
+print(f"Part 2: {sum(powers)}")
