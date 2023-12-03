832622db7ab3deb204df3fc919445864b7c0f4e5
JarroVGIT <jarro@vanginkel-it.nl>
2023-12-03 10:34:53 +0100

    Day 3 solution

--- day03/day03-part 1.py
+++ day03/day03-part 1.py
@@ -5,9 +5,39 @@
 # Possible imports required:
 # import pandas as pd
 # import numpy as np
-# import re
+import re
 from rich import print
+from collections import defaultdict
 
-with open("./day03/example.txt") as f:
-    # with open("./day03/input.txt") as f:
-    content = f.read().split("\n")
+# with open("./day03/example.txt") as f:
+with open("./day03/input.txt") as f:
+    content = f.read()
+content_in_lines = content.split("\n")
+
+char_locations = set(
+    [
+        (row, column)
+        for row in range(len(content_in_lines))
+        for column in range(len(content_in_lines[row]))
+        if content_in_lines[row][column] not in ".1234567890"
+    ]
+)
+
+numbers_to_chars = defaultdict(list)
+for row, line in enumerate(content_in_lines):
+    numbers = re.finditer(r"\d+", line)
+    number: re.Match
+    for number in numbers:
+        neighbors = set(
+            [
+                (row + row_offset, column + column_offset)
+                for row_offset in range(-1, 2)
+                for column_offset in range(-1, 2)
+                for column in range(number.span()[0], number.span()[1])
+            ]
+        )
+        for char in neighbors & char_locations:
+            numbers_to_chars[char].append(int(number[0]))
+
+ans = sum([sum(numbers_to_char) for numbers_to_char in numbers_to_chars.values()])
+print("Part 1: ", ans)
--- day03/day03-part 2.py
+++ day03/day03-part 2.py
@@ -5,9 +5,46 @@
 # Possible imports required:
 # import pandas as pd
 # import numpy as np
-# import re
+import re
 from rich import print
+from collections import defaultdict
 
-with open("./day03/example.txt") as f:
-    # with open("./day03/input.txt") as f:
-    content = f.read().split("\n")
+# with open("./day03/example.txt") as f:
+with open("./day03/input.txt") as f:
+    content = f.read()
+content_in_lines = content.split("\n")
+
+char_locations = set(
+    [
+        (row, column)
+        for row in range(len(content_in_lines))
+        for column in range(len(content_in_lines[row]))
+        if content_in_lines[row][column] == "*"
+    ]
+)
+
+numbers_to_chars = defaultdict(list)
+for row, line in enumerate(content_in_lines):
+    numbers = re.finditer(r"\d+", line)
+    number: re.Match
+    for number in numbers:
+        neighbors = set(
+            [
+                (row + row_offset, column + column_offset)
+                for row_offset in range(-1, 2)
+                for column_offset in range(-1, 2)
+                for column in range(number.span()[0], number.span()[1])
+            ]
+        )
+        for char in neighbors & char_locations:
+            numbers_to_chars[char].append(int(number[0]))
+
+ans = sum(
+    [
+        numbers_to_char[0] * numbers_to_char[1]
+        for numbers_to_char in numbers_to_chars.values()
+        if len(numbers_to_char) == 2
+    ]
+)
+
+print("Part 2: ", ans)
