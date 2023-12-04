1733c74857883d7ade79c72ae7cdf48f5d21f28d
JarroVGIT <jarro@vanginkel-it.nl>
2023-12-04 08:10:35 +0100

    Day 4 solution

--- day04/day04-part 1.py
+++ day04/day04-part 1.py
@@ -8,6 +8,21 @@
 # import re
 from rich import print
 
-with open("./day04/example.txt") as f:
-    # with open("./day04/input.txt") as f:
+# with open("./day04/example.txt") as f:
+with open("./day04/input.txt") as f:
     content = f.read().split("\n")
+
+winning_numbers_list = []
+card_numbers_list = []
+
+for line in content:
+    numbers = line.split(":")[1]
+    winning_numbers_list.append([int(n) for n in numbers.split("|")[0].split()])
+    card_numbers_list.append([int(n) for n in numbers.split("|")[1].split()])
+
+total_win = 0
+for idx, winning_number in enumerate(winning_numbers_list):
+    count = len(set(winning_number) & set(card_numbers_list[idx]))
+    total_win += 2 ** (count - 1) if count > 1 else 0 if count == 0 else 1
+
+print(total_win)
--- day04/day04-part 2.py
+++ day04/day04-part 2.py
@@ -7,7 +7,37 @@
 # import numpy as np
 # import re
 from rich import print
+from collections import defaultdict
 
-with open("./day04/example.txt") as f:
-    # with open("./day04/input.txt") as f:
+# with open("./day04/example.txt") as f:
+with open("./day04/input.txt") as f:
     content = f.read().split("\n")
+
+
+winning_numbers_list = []
+card_numbers_list = []
+winning_numbers_dict = {}
+card_numbers_dict = {}
+cards_processed = defaultdict(lambda: 0)
+
+for line in content:
+    card_id = int(line.split(":")[0].split()[1])
+    numbers = line.split(":")[1]
+
+    winning_numbers_dict[card_id] = [int(n) for n in numbers.split("|")[0].split()]
+    card_numbers_dict[card_id] = [int(n) for n in numbers.split("|")[1].split()]
+    # tuple of card_id and list of numbers
+    card_numbers_list.append((card_id, [int(n) for n in numbers.split("|")[1].split()]))
+
+total_win = 0
+while card_numbers_list:
+    card_id, card_numbers = card_numbers_list.pop(0)
+    cards_processed[card_id] += 1
+    winning_numbers = winning_numbers_dict[card_id]
+    count = len(set(winning_numbers) & set(card_numbers))
+    cards_to_add = [
+        (card_id + i, card_numbers_dict[card_id + i]) for i in range(1, count + 1)
+    ]
+    card_numbers_list.extend(cards_to_add)
+
+print(sum([v for k, v in cards_processed.items()]))
