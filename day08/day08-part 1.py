18bd739a5e179e87b80a7ce447a7731e13f3c4b0
JarroVGIT <jarro@vanginkel-it.nl>
2023-12-08 08:54:47 +0100

    Day 8 solutions

--- day08/day08-part 1.py
+++ day08/day08-part 1.py
@@ -5,9 +5,25 @@
 # Possible imports required:
 # import pandas as pd
 # import numpy as np
-# import re
+import re
 from rich import print
-
-with open("./day08/example.txt") as f:
-    # with open("./day08/input.txt") as f:
+from itertools import cycle
+# with open("./day08/example.txt") as f:
+with open("./day08/input.txt") as f:
     content = f.read().split("\n")
+
+leftright = content[0]
+
+nodes = {}
+for line in content[2:]:
+    # find all three letters in line
+    node, l, r = re.findall(r"[A-Z]{3}", line)
+    nodes[node] = {"L": l, "R": r}
+
+node = 'AAA'
+steps = 0
+for step, instruction in enumerate(cycle(leftright)):
+    node = nodes[node][instruction]
+    if node == 'ZZZ':
+        print(f"Step: {step+1}")
+        break
--- day08/day08-part 2.py
+++ day08/day08-part 2.py
@@ -5,9 +5,33 @@
 # Possible imports required:
 # import pandas as pd
 # import numpy as np
-# import re
+import re
 from rich import print
+from itertools import cycle
+from math import lcm
 
-with open("./day08/example.txt") as f:
-    # with open("./day08/input.txt") as f:
+#with open("./day08/example-2.txt") as f:
+with open("./day08/input.txt") as f:
     content = f.read().split("\n")
+
+leftright = content[0]
+
+nodes = {}
+for line in content[2:]:
+    # find all three letters in line
+    node, l, r = re.findall(r"[0-9A-Z]{3}", line)
+    nodes[node] = {"L": l, "R": r}
+
+start_nodes = [node for node in nodes if node[-1] == 'A']
+
+steps_until_z = [] # For each starting node, how many steps until Z
+for node in start_nodes:
+    for idx, instruction in enumerate(cycle(leftright)):
+        node = nodes[node][instruction]
+        if node[-1] == 'Z':
+            steps_until_z.append(idx + 1)
+            break
+
+# Answer is the lowest common multiple of all steps_until_z
+print(lcm(*steps_until_z))
+
