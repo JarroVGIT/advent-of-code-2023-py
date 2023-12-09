c9ef338ef4eb135259eb0976b32c81f79f14dff8
JarroVGIT <jarro@vanginkel-it.nl>
2023-12-09 09:08:55 +0100

    Day 9 solution with comments on part 1

--- day09/day09-part 1.py
+++ day09/day09-part 1.py
@@ -8,6 +8,45 @@
 # import re
 from rich import print
 
-with open("./day09/example.txt") as f:
-    # with open("./day09/input.txt") as f:
+#with open("./day09/example.txt") as f:
+with open("./day09/input.txt") as f:
     content = f.read().split("\n")
+
+content = [[int(x) for x in line.split()] for line in content]
+
+def calculate_differences(sequence: list[int]) -> list[int]:
+    # Calculate the differences between each number in a sequence of ints
+    return [j-i for i, j in zip(sequence[:-1], sequence[1:])]
+
+def predict_next(sequence: list[int]) -> list[int]:
+    # We are only interested in the last number of each sequence and subsequent difference-sequences
+    last_numbers = [sequence[-1]]
+
+    # Create a first new sequence (of differences) from the original sequence.
+    differences = calculate_differences(sequence)
+
+    # Keep doing so, untill all differences are 0
+    while not all([n == 0 for n in differences]):
+        # Store the last number of this new sequence
+        last_numbers.append(differences[-1])
+        # Create a new sequence (of differences) from the previous sequence
+        differences = calculate_differences(differences)
+    
+    # The final 'last_number' must be 0.
+    last_numbers.append(0)
+    new_last_numbers = []
+    for i, n in enumerate(reversed(last_numbers)):
+        if i == 0:
+            # The first number is always 0
+            new_last_numbers.append(n)
+            continue
+        # The next last_number is the previous last_number plus the current number
+        new_last_numbers.append(new_last_numbers[-1] + n)
+    sequence.append(new_last_numbers[-1])
+    return sequence
+
+new_content = []
+for line in content:
+    new_content.append(predict_next(line))
+
+print(sum([x[-1] for x in new_content]))
--- day09/day09-part 2.py
+++ day09/day09-part 2.py
@@ -8,6 +8,34 @@
 # import re
 from rich import print
 
-with open("./day09/example.txt") as f:
-    # with open("./day09/input.txt") as f:
+#with open("./day09/example.txt") as f:
+with open("./day09/input.txt") as f:
     content = f.read().split("\n")
+
+content = [[int(x) for x in line.split()] for line in content]
+
+def calculate_differences(sequence: list[int]) -> list[int]:
+    return [j-i for i, j in zip(sequence[:-1], sequence[1:])]
+
+def predict_prev(sequence: list[int]) -> list[int]:
+    first_numbers = [sequence[0]]
+    differences = calculate_differences(sequence)
+    while not all([n == 0 for n in differences]):
+        first_numbers.append(differences[0])
+        differences = calculate_differences(differences)
+    
+    first_numbers.append(0)
+    new_first_numbers = []
+    for i, n in enumerate(reversed(first_numbers)):
+        if i == 0:
+            new_first_numbers.append(n)
+            continue
+        new_first_numbers.append(n - new_first_numbers[-1])
+    sequence.insert(0, new_first_numbers[-1])
+    return sequence
+
+new_content = []
+for line in content:
+    new_content.append(predict_prev(line))
+
+print(sum([x[0] for x in new_content]))
