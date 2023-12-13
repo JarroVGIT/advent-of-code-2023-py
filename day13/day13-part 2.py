2e7e8c6c3d2ae37887105e64a3f6d8785ffee58d
Jarro van Ginkel <rob7609@robeco.nl>
2023-12-13 19:28:49 +0100

    Day 13 part 2 done!

--- day13/day13-part 2.py
+++ day13/day13-part 2.py
@@ -8,9 +8,9 @@
 # import re
 from rich import print
 
-with open("./day13/example.txt") as f:
-    # with open("./day13/input.txt") as f:
-    content = f.read().split("\n")
+# with open("./day13/example.txt") as f:
+with open("./day13/input.txt") as f:
+    content = f.read().split("\n\n")
 
 # need to go line by line, first check if there is a match. If there is not 
 # a match, check if one change (smutch) makes a difference if there is not already a smutch used.
@@ -23,62 +23,48 @@ for block in content:
 
     grids.append(grid)
 
-def compare_row_sets(row_set_1, row_set_2, with_smutch: bool) -> bool:
-
-
-    if not smutch_allowed:
-        return row1 == row2
-    else:
-        # Check if there is one smutch:
-        not_same = [idx for idx, i, j in enumerate(zip(row1, row2)) if i != j]
-        if len(not_same) == 1: 
+def compare_row_sets(row_set_1, row_set_2, smutch_used: bool) -> bool:
+    if not row_set_1:
+        # Exhausted both row_sets. 
+        if smutch_used:
             return True
-        else:
+        else: 
+            # Must fix a smutch!
             return False
+    
+    if row_set_1[0] == row_set_2[0]:
+        return compare_row_sets(row_set_1[1:], row_set_2[1:], smutch_used)
+    else:
+        if not smutch_used:
+            not_same_idx = [(i, j) for i, j in zip(row_set_1[0], row_set_2[0]) if i != j]
+            if len(not_same_idx) == 1:
+                return compare_row_sets(row_set_1[1:], row_set_2[1:], True)
+        return False
 
 def overlapping_rows(grid):
-    smutch_used = False
-
-    intermediate_score = 0
-
     for row_id in range(len(grid)-1):
-        # First check if there is a potential match, otherwise continue:
-        if compare_row_sets([grid[row_id]], [grid[row_id+1]], False):
-            # Check if rest of rows is mirrored as well, without smutch. 
-            # Create row set 1 and 2, and feed into compare rowsets.
-            pass # some recursive function to do here.
+        # Create rowsets to compare:
+        if row_id < len(grid)//2:
+            row_set_1 = grid[ : row_id+1 ]
+            row_set_2 = grid[ (row_id*2)+1 : row_id : -1 ]
+        else:
+            row_set_1 = grid[ row_id+1 : ]
+            row_set_2 = grid[ row_id : row_id-(len(grid)-row_id) + 1 : -1 ]
 
-        elif compare_row_sets(grid[row_id], grid[row_id+1], True):
-            # Check if rest of rows is mirrored as well, with smutch.
-            pass # some recursive function to do here.
+        # Compare:
+        if compare_row_sets(row_set_1, row_set_2, False):
+                return row_id+1
         else:
             continue
-
-
-
-
-    for row_id in range(len(grid)):
-        if (row_id+1<len(grid)) and  grid[row_id] == grid[row_id+1]:
-            # Check if rest of rows is mirrored as well:
-            if row_id < len(grid)//2:
-                # Ignore last rows
-                if grid[ : row_id+1 ] == grid[ (row_id*2)+1 : row_id : -1 ]:
-                    intermediate_score += row_id+1
-                    break
-            else:
-                # Ignore first rows
-                if grid[ row_id+1 : ] == grid[ row_id : row_id-(len(grid)-row_id) + 1 : -1 ]:
-                    intermediate_score += row_id+1
-                    break
-    return intermediate_score
-
+    # If this hits, it means we should look at columns instead.
+    return 0
 
 
 score = 0
 for idx, grid in enumerate(grids):
     s = overlapping_rows(grid) * 100
     if not s:
-        transposed = list(map(list, zip(*grid)))
+        transposed = list(map(list, zip(*grid))) # Man I love this one!
         s = overlapping_rows(transposed)
     score += s
 
