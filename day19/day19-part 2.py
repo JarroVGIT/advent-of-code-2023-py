ea2fab0058f2e641da20f9ebdeaa5968da4a8c9d
JarroVGIT <jarro@vanginkel-it.nl>
2023-12-19 22:33:13 +0100

    Day 19, Part 2, after a LOT of off-by-one's..

--- day19/day19-part 2.py
+++ day19/day19-part 2.py
@@ -10,8 +10,8 @@ import re
 from rich import print
 from dataclasses import dataclass
 from typing import Literal
-with open("./day19/example.txt") as f:
-#with open("./day19/input.txt") as f:
+#with open("./day19/example.txt") as f:
+with open("./day19/input.txt") as f:
     content = f.read().split("\n\n")
 
 @dataclass
@@ -28,7 +28,6 @@ operation_mapping = {
 }
 
 workflows_raw = content[0].split("\n")
-parts_raw = content[1].split("\n")    
 
 workflows: dict[str, list[Rule]] = {}
 pattern = r"(?P<workflow>\w+)\{(?P<rules>.*)\}"
@@ -46,4 +45,60 @@ for wf in workflows_raw:
     workflows[workflow] = current_rules
 
 
-print(workflows)
+r_obj = {
+    'x': (1, 4000),
+    'm': (1, 4000),
+    'a': (1, 4000),
+    's': (1, 4000)
+}
+
+def calculate_combinations(r_obj: dict[str, tuple[int, int]]) -> int:
+    c = [r[1] - r[0] + 1 for r in r_obj.values()]
+    return c[0] * c[1] * c[2] * c[3]
+    # for r in r_obj.values():
+    #     total *= r[1] - r[0] + 1
+    # return total
+
+def process_range(workflow: str, r_obj: dict[str, tuple[int, int]], rule_idx: int = 0) -> int:
+    # Assert rule for ranges, and determine which part will move to new rule. If R then return 0, 
+    # if Accepted return the total possible combinations of the ranges. 
+    if workflow == "A":
+            return calculate_combinations(r_obj)
+    elif workflow == "R":
+            return 0
+    rule = workflows[workflow][rule_idx]
+    if rule.attr is None and rule.operation is None and rule.value is None:
+        return process_range(rule.go_to_workflow, r_obj)
+    else: 
+        # Check if we have to split up the range.
+        r_start, r_end = r_obj[rule.attr]
+        if rule.operation == '<':
+            if r_end < rule.value:
+                # entire range moves to next workflow.
+                return process_range(rule.go_to_workflow, {**r_obj})
+            elif r_start < rule.value:
+                # split up the range.
+                matched_r = {**r_obj}
+                not_matched_r = {**r_obj}
+                not_matched_r[rule.attr] = (rule.value, r_end)
+                matched_r[rule.attr] = (r_start, rule.value - 1)
+                return (process_range(workflow, not_matched_r, rule_idx+1) +
+                        process_range(rule.go_to_workflow, matched_r))
+            else:
+                # Entire range move to next rule
+                return process_range(rule.go_to_workflow, {**r_obj}, rule_idx+1)
+        elif rule.operation == '>':
+            if r_start > rule.value:
+                # entire range moves to next workflow.
+                return process_range(rule.go_to_workflow, {**r_obj})
+            elif r_end > rule.value:
+                matched_r = {**r_obj}
+                not_matched_r = {**r_obj}
+                matched_r[rule.attr] = (rule.value + 1, r_end)
+                not_matched_r[rule.attr] = (r_start, rule.value)
+                return (process_range(workflow, not_matched_r, rule_idx+1) +
+                        process_range(rule.go_to_workflow, matched_r))
+            else:
+                return process_range(rule.go_to_workflow, r_obj, rule_idx+1) 
+
+print(process_range("in", r_obj))
