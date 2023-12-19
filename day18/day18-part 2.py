405dc1c1508909f0b98aeab0d65109b782cacfb3
JarroVGIT <jarro@vanginkel-it.nl>
2023-12-19 08:15:55 +0100

    should work but slow (d18 still, p2)

--- day18/day18-part 2.py
+++ day18/day18-part 2.py
@@ -7,9 +7,11 @@
 # import numpy as np
 import re
 from rich import print
-from collections import deque
-with open("./day18/example.txt") as f:
-    # with open("./day18/input.txt") as f:
+
+from dataclasses import dataclass
+from typing import Literal
+#with open("./day18/example.txt") as f:
+with open("./day18/input.txt") as f:
     content = f.read().split("\n")
 
 direction_mapping = {
@@ -25,60 +27,119 @@ for line in content:
     distance = int(match.group("distance"), 16)
     instructions.append((direction_mapping[int(match.group('direction'))], distance))
 
-print(instructions)
+@dataclass
+class Point:
+    x: int
+    y: int
 
+@dataclass
+class Line:
+    start: Point # lowest x or lowest y
+    end: Point
+    direction: Literal["H", "V"]
+    length: int
+    range: range
 
-direction_mapping = {
-    'R': 1,
-    'L': -1,
-    'U': -1j, # Reversed grid
-    'D': 1j
-}
+hranges: list[Line] = []
+vranges: list[Line] = []
 
+current_point = Point(0,0)
 
-# Create ranges of lines (U/D and L/R) with tuple (y, range(x1, x2)) or (x, range(y1, y2))
-# Then, take max_y, min_y, max_x, min_x and loop over each Y (line). 
-# Only count x's that are in between 2 vertical ranges at the time. 
+min_x = 0
+min_y = 0
+max_x = 0
+max_y = 0
 
-
-# Create coordinates of trenches
-trench_coordinates = [complex(0,0)]
-current_coorindate = 0+0j
-for instruction in instructions:
+for i, instruction in enumerate(instructions):
     direction, length = instruction
-    for i in range(length):
-        current_coorindate += direction_mapping[direction]
-        trench_coordinates.append(current_coorindate)
+    if direction == "R":
+        # 0,0, 3 lenght -> 3,0
+        new_point = Point(current_point.x + length, current_point.y)
+        hranges.append(Line(current_point, new_point, "H", length, range(current_point.x, current_point.x + length + 1)))
+        current_point = new_point
+        min_x, max_x = min(min_x, current_point.x), max(max_x, current_point.x)
+        min_y, max_y = min(min_y, current_point.y), max(max_y, current_point.y)
+    elif direction == "L":
+        # 0,0, 3 lenght -> -3,0
+        new_point = Point(current_point.x - length, current_point.y)
+        hranges.append(Line(new_point, current_point, "H", length, range(current_point.x - length, current_point.x + 1)))
+        current_point = new_point
+        min_x, max_x = min(min_x, current_point.x), max(max_x, current_point.x)
+        min_y, max_y = min(min_y, current_point.y), max(max_y, current_point.y)
+    elif direction == "U":
+        # 0,0, 3 lenght -> 0,-3
+        new_point = Point(current_point.x, current_point.y - length)
+        vranges.append(Line(new_point, current_point, "V", length, range(current_point.y - length, current_point.y + 1)))
+        current_point = new_point
+        min_x, max_x = min(min_x, current_point.x), max(max_x, current_point.x)
+        min_y, max_y = min(min_y, current_point.y), max(max_y, current_point.y)
+    elif direction == "D":
+        # 0,0, 3 lenght -> 0,3
+        new_point = Point(current_point.x, current_point.y + length)
+        vranges.append(Line(current_point, new_point, "V", length, range(current_point.y, current_point.y + length + 1)))
+        current_point = new_point
+        min_x, max_x = min(min_x, current_point.x), max(max_x, current_point.x)
+        min_y, max_y = min(min_y, current_point.y), max(max_y, current_point.y)    
+    else:
+        raise Exception("Unknown direction")
 
-# Create total grid
-max_x = int(max([c.real for c in trench_coordinates]))
-max_y = int(max([c.imag for c in trench_coordinates]))
-min_x = int(min([c.real for c in trench_coordinates]))
-min_y = int(min([c.imag for c in trench_coordinates]))
+filled = 0
 
-grid = []
-for y in range(min_y-1, max_y+2): # At least a 1x1 border.
-    for x in range(min_x-1, max_x+2):
-        grid.append(complex(x, y))
+for y in range(min_y, max_y+1):
+    # All vertical lines crossing this Y
+    vertical_lines = sorted([vrange for vrange in vranges if y in vrange.range], key=lambda x: x.start.x)
+    # All horizontal lines on this Y
+    horizontal_lines = sorted([hrange for hrange in hranges if y == hrange.start.y], key=lambda x: x.start.x)
+    
+    x_start = None
+    h_line_idx = None
+    hl_starts = [hline.start.x for hline in horizontal_lines]
 
-# Flood it, remove all non-trench coordinates
+    for v_idx, vline in enumerate(vertical_lines):
+        if x_start == None:
+            x_start = vline.start.x
+            if x_start in hl_starts:
+                h_line_idx = hl_starts.index(x_start)
+            continue
+        elif h_line_idx != None and x_start != None: 
+            # We have a starting line, but are following a horizontal line.
+            # If second line is going same direction als prev line, then count
+            # the horizontal line as filled and reset x_start and h_line
+            prev_line = vertical_lines[v_idx - 1]
+            if prev_line.start.y == vline.start.y or prev_line.end.y == vline.end.y:
+                if prev_line.start.x != x_start:
+                    # This is a top end in a counting line, prev-line is not the start. Just continue.
+                    h_line_idx = None
+                    continue
+                else:   
+                    # Same direction
+                    filled += vline.start.x - x_start + 1
+                    # print(f"{y}: {vline.start.x} - {x_start} + 1 = {vline.start.x - x_start + 1}")
+                    x_start = None
+                    last_h_line_idx = h_line_idx
+                    h_line_idx = None
+                    continue
+            else:
+                if prev_line.start.x != x_start:
+                    filled += vline.start.x - x_start+ 1
+                    # print(f"{y}: {vline.start.x} - {x_start} + 1 = {vline.start.x - x_start + 1}")
+                    x_start = None
+                    h_line_idx = None
+                else:
+                    # Different direction, keep going and ignore line.
+                    h_line_idx = None
+                    continue
+        else:
+            # We have a starting line, but not following a horizontal line.
+            # But if second vline starts/stops at horizontal line, keep going. 
+            if vline.start.x in hl_starts:
+                h_line_idx = hl_starts.index(vline.start.x)
+                continue
+            else:
+                # Second line does not start/stop at horizontal line, so count first line as filled.
+                filled += vline.start.x - x_start+ 1
+                # print(f"{y}: {vline.start.x} - {x_start} + 1 = {vline.start.x - x_start + 1}")
+                x_start = None
+                continue
 
-passed = set()
-
-q = deque([complex(min_x-1,min_y-1)])
-neighbors = [1, -1, 1j, -1j]
-while len(q) > 0:
-    current = q.popleft()
-    if current in passed:
-        continue
-    passed.add(current)
-    if current not in grid:
-        continue
-    grid.remove(current)
-
-    for direction in [1, -1, 1j, -1j]:
-        new = current + direction
-        if new in grid and new not in trench_coordinates:
-            q.append(current + direction)
-
-print(len(grid))
+print(filled)
