ef5cff0cd2003b69a941925134964e47955963ad
JarroVGIT <jarro@vanginkel-it.nl>
2023-12-21 12:07:04 +0100

    Day 21 p1

--- day21/day21-part 1.py
+++ day21/day21-part 1.py
@@ -8,6 +8,46 @@
 # import re
 from rich import print
 
-with open("./day21/example.txt") as f:
-    # with open("./day21/input.txt") as f:
+#with open("./day21/example.txt") as f:
+with open("./day21/input.txt") as f:
     content = f.read().split("\n")
+
+# Store all tiles, then create a dict with adjecent tiles.
+
+start = None
+tiles = {}
+bushes = {}
+for y, row in enumerate(content):
+    for x, val in enumerate(row):
+        if val == 'S':
+            start = complex(x, y)
+            tiles[complex(x, y)] = set()
+        elif val == '.':
+            tiles[complex(x, y)] = set()
+        else:
+            bushes[complex(x, y)] = set()
+
+for tile in tiles.keys():
+    for direction in [1, -1, 1j, -1j]:
+        if tile + direction in tiles:
+            tiles[tile].add(tile + direction)
+
+# @cache
+# def get_neighbours(current_tile, steps_left):
+#     if steps_left == 1:
+#         return tiles[current_tile]
+#     else:
+#         next_tiles = []
+#         for neighbour in tiles[current_tile]:
+#             next_tiles.extend(get_neighbours(neighbour, steps_left - 1))
+#         return next_tiles
+
+def get_next_possible_steps(current_tiles: set[complex], steps_left: int):
+    if steps_left == 0:
+        return current_tiles
+    next_tiles = set()
+    for tile in current_tiles:
+        next_tiles = next_tiles | tiles[tile]
+    return get_next_possible_steps(next_tiles, steps_left - 1)
+
+print(len(get_next_possible_steps([start], 64)))
