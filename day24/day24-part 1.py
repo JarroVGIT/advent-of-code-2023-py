b760d7dd85500fc46845972aeb3b799ac71de68e
JarroVGIT <jarro@vanginkel-it.nl>
2023-12-27 20:29:33 +1100

    Day 24 and day 25

--- day24/day24-part 1.py
+++ day24/day24-part 1.py
@@ -5,9 +5,73 @@
 # Possible imports required:
 # import pandas as pd
 # import numpy as np
-# import re
+import re
 from rich import print
-
-with open("./day24/example.txt") as f:
-    # with open("./day24/input.txt") as f:
+from dataclasses import dataclass
+import itertools
+#with open("./day24/example.txt") as f:
+with open("./day24/input.txt") as f:
     content = f.read().split("\n")
+
+@dataclass
+class Hailstone:
+    id: int
+    x: int
+    y: int
+    z: int
+    dx: int
+    dy: int
+    dz: int
+    a: int | None = None
+    b: int | None = None
+
+hail_stones: list[Hailstone] = []
+
+
+pattern = r'-?\d+'
+for id, line in enumerate(content):
+    x, y, z, dx, dy, dz = map(int, re.findall(pattern, line))
+    hail_stones.append(Hailstone(id, x, y, z, dx, dy, dz))
+
+
+# Need lineair functions for each hailstone (y=ax+b)
+for hailstone in hail_stones:
+    # Each nanosecond, dx and dy happens. This means: if x increases with dx, then y increases with dy.
+    # So, if we want dy for dx=1 (=a), then a = dy/dx
+    hailstone.a = hailstone.dy / hailstone.dx
+    # y = a * x + b, we know y, x and a, so we can calculate b
+    hailstone.b = hailstone.y - hailstone.a * hailstone.x
+
+def intersection(h1: Hailstone, h2: Hailstone) -> tuple[float, float] | None:
+    # Calculate the intersection of 2 hailstones
+    # y = a * x + b
+    # y = c * x + d
+    # a * x + b = c * x + d
+    # a * x - c * x = d - b
+    # x * (a - c) = d - b
+    # x = (d - b) / (a - c)
+    if h1.a == h2.a:
+        # Parallel lines
+        return None
+
+    x = (h2.b - h1.b) / (h1.a - h2.a)
+    y = h1.a * x + h1.b
+    return (x, y)
+
+test_min_x, test_max_x = 200000000000000, 400000000000000
+test_min_y, test_max_y = 200000000000000, 400000000000000
+
+count = 0
+for combination in itertools.combinations(hail_stones, 2):
+    # Check if the hailstones intersect
+    intersection_point = intersection(combination[0], combination[1])
+    if intersection_point is not None:
+        x, y = intersection_point
+        if ((test_min_x <= x <= test_max_x) and (test_min_y <= y <= test_max_y)
+            and ((combination[0].x < x and combination[0].dx > 0) or (combination[0].x > x and combination[0].dx < 0))
+            and ((combination[0].y < y and combination[0].dy > 0) or (combination[0].y > y and combination[0].dy < 0))
+            and ((combination[1].x < x and combination[1].dx > 0) or (combination[1].x > x and combination[1].dx < 0))
+            and ((combination[1].y < y and combination[1].dy > 0) or (combination[1].y > y and combination[1].dy < 0))
+            ):
+            count += 1
+print(count)
--- day25/day25-part 1.py
+++ day25/day25-part 1.py
@@ -5,9 +5,25 @@
 # Possible imports required:
 # import pandas as pd
 # import numpy as np
-# import re
+import re
 from rich import print
-
-with open("./day25/example.txt") as f:
-    # with open("./day25/input.txt") as f:
+from collections import defaultdict
+import itertools
+import networkx as nx
+#with open("./day25/example.txt") as f:
+with open("./day25/input.txt") as f:
     content = f.read().split("\n")
+
+pattern = r'[a-z]{3}'
+nondirectional_connections: list[set[str]] = []
+directional_connections: dict[str, list[str]] = defaultdict(list)
+graph = nx.Graph()
+for line in content:
+    matches = re.findall(pattern, line)
+    for i in range(1, len(matches)):
+        graph.add_edge(matches[0], matches[i])
+
+cut_value, partition = nx.stoer_wagner(graph)
+
+print(cut_value)
+print(len(partition[0])*len(partition[1]))
